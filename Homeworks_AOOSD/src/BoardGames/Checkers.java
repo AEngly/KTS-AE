3935da876c4ceae78720651ad89fa8c9ec9c746c
andreasengly <andreasengly@192.168.249.49>
2019-03-03 17:41:35 +0100

    k

--- Homeworks_AOOSD/src/BoardGames/Checkers.java
+++ Homeworks_AOOSD/src/BoardGames/Checkers.java
@@ -4,16 +4,37 @@ import java.util.Scanner;
 
 public class Checkers extends BoardGames {
 	Player Turn = new Player("White");
-	Player[] players = {new Player("Black"),new Player("White")};
-	Board board = new Board(8,8);
-	Piece[] pieces = ; 
-	
-	public static void setup(Board board) {}
-	
+	Player[] players = { new Player("Black"), new Player("White") };
+	Board board = new Board(8, 8);
+	Piece[] pieces = ;
+
+	public static void setup(Board board) {
+	}
+
 	public static void display(Board board) {
 		Display(board());
 	}
-	
+
+	public static Piece[] getAllPieces(Player[] players) {
+
+		int no_pieces = 0;
+
+		for (int i = 0; i < players.length; i++) {
+			no_pieces = no_pieces + players[i].pieces.length;
+		}
+
+		Piece[] allPieces = new Piece[no_pieces];
+
+		for (int i = 0; i < players.length; i++) {
+			for (int j = 0; j < players[i].pieces.length; j++) {
+				allPieces[j + i * players[i].pieces.length] = players[i].pieces[j];
+			}
+		}
+
+		return allPieces;
+
+	}
+
 	public static void turn(Player[] players, Player Turn) {
 
 		
@@ -39,35 +60,27 @@ public class Checkers extends BoardGames {
 
 	private static Pair<Character, Integer>[] Moveinput() {
 		Scanner s = new Scanner(System.in);
-	  	char x;
+		char x;
 		int y;
-	  	char nx;
+		char nx;
 		int ny;
 
-			System.out.println("Turn of player no. 1 \n");
-			System.out.println("Coordinate of piece to move \n");
-			System.out.println("Enter X:");
-			x = s.next().charAt(0);
-			System.out.println("\nEnter Y:");
-			y = s.nextInt();
-			System.out.println("\nCoordinate of new position \n");
-			System.out.println("Enter X:");
-			nx = s.next().charAt(0);
-			System.out.println("\nEnter Y:");
-			ny = s.nextInt();
-			Pair<Character,Integer> old = new Pair<>(x,y);
-			Pair<Character,Integer> next = new Pair<>(nx,ny);
-			Pair<Character, Integer>[] move = new Pair[] {old,next};
+		System.out.println("Turn of player no. 1 \n");
+		System.out.println("Coordinate of piece to move \n");
+		System.out.println("Enter X:");
+		x = s.next().charAt(0);
+		System.out.println("\nEnter Y:");
+		y = s.nextInt();
+		System.out.println("\nCoordinate of new position \n");
+		System.out.println("Enter X:");
+		nx = s.next().charAt(0);
+		System.out.println("\nEnter Y:");
+		ny = s.nextInt();
+		Pair<Character, Integer> old = new Pair<>(x, y);
+		Pair<Character, Integer> next = new Pair<>(nx, ny);
+		Pair<Character, Integer>[] move = new Pair[] { old, next };
 		// TODO Auto-generated method stub
 		return move;
 	}
-
-
-		
-		
-		
-
-
-
 
 }
