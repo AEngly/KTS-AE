82bb9d9980edb9f694c240465af9bd8149e9f021
andreasengly <andreasengly@192.168.0.3>
2019-03-08 18:44:20 +0100

    Comments

--- Homeworks_AOOSD/src/BoardGame/CheckerPlayer.java
+++ Homeworks_AOOSD/src/BoardGame/CheckerPlayer.java
@@ -2,10 +2,9 @@ package BoardGame;
 
 public class CheckerPlayer extends Player {
 
-	//Use enum for colour
-	
-	public CheckerPlayer(String input) {
-		super(input);
-	}
+	// Use enum for colour
 
+	public CheckerPlayer(String input) {
+		super(input); // Calls player constructor and sets the colour of the player accordingly.
+	}
 }
